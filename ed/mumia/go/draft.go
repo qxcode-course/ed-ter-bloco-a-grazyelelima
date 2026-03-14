e3daa916bf5602bafa762be73b402f489240dc75
grazyelelima <grazyelelima028@gmail.com>
2026-03-14 12:44:41 -0300

    Resolução mumia

---
+++ ed/mumia/go/draft.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+func main() {
+    var name string
+    var age int
+
+    fmt.Scan(&name)
+    fmt.Scan(&age)
+
+    if age < 12 {
+        fmt.Printf("%s eh crianca\n", name)
+    } else if age < 18 {
+        fmt.Printf("%s eh jovem\n", name)
+    } else if age < 65 {
+        fmt.Printf("%s eh adulto\n", name)
+    } else if age < 1000 {
+        fmt.Printf("%s eh idoso\n", name)
+    } else {
+        fmt.Printf("%s eh mumia\n", name)
+    }
+}
