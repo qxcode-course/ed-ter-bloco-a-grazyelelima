1c1c3044f4a03b2c189275d098883cddb2ea25c2
grazyelelima <grazyelelima028@gmail.com>
2026-06-20 12:52:52 -0300

    embua

--- ed/fractais_png/src/go/draft.go
+++ ed/fractais_png/src/go/draft.go
@@ -9,19 +9,24 @@ func rand_int(inf, sup int) float64 {
 	return float64(rand.Intn(sup-inf+1) + inf)
 }
 
+func embua(pen *Pen, dist float64) {
+	if dist < 1{
+		return
+	}
+
+	pen.SetLineWidth(dist / 30)
+	pen.Walk(dist)
+	pen.Right(90)
+	dist *= 0.97
+	embua(pen, dist)
+}
 
 func main() {
 	pen := NewPen(500, 500)
 	pen.SetRGB(0, 0, 200)
-	pen.SetPosition(250, 250)
 	pen.SetHeading(0)
-	pen.Walk(100)
-	pen.Right(90)
-	pen.SetRGB(200, 0, 0)
-	pen.Walk(200)
-	pen.Right(30)
-	pen.Walk(250)
-
-	pen.SavePNG("tree.png")
+	pen.SetPosition(100, 100)
+	embua(pen, 300)
+	pen.SavePNG("embua.png")
 	fmt.Println("PNG file created successfully.")
 }
